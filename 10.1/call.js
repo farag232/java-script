e77770fe75bac2811d833ccb1877ff35f3c2ba04
farag232 <faragfadool232@gmail.com>
2022-10-02 21:48:27 +0300

    second message

---
+++ 10.1/call.js
@@ -0,0 +1,49 @@
+function isString(str, func){
+    if(typeof str === 'string'){
+        return func(str);
+    }
+    return "Not a string";
+}
+
+function print(str){
+    console.log(str);
+}
+
+//! test
+isString("hello", print);
+console.log(isString(555, print));
+
+
+//$2 
+
+function firstWordUpperCase(str, func){
+    str = str.replace(str[0], str[0].toUpperCase());
+    return func(str);
+}
+
+function dash(str){
+    str = str.replace(/\s/g, "-");
+    console.log(str);
+}
+
+//! test
+
+firstWordUpperCase("hello world wahooo", dash);
+
+//$3
+
+function myFunc(str){
+    str = str.replace("a","$");
+    console.log(str);
+}
+
+firstWordUpperCase("hello world wahooo", myFunc);
+
+//$4 replace the first "a" with "$"
+
+function replacment(str, func){
+
+    return func(str);
+}
+
+replacment("halllo ma friend", myFunc);
---
+++ 3.2/dad.js
@@ -0,0 +1,27 @@
+function welcome() {
+    let welcome = 'Welcome to Appleseeds Bootcamp!';
+    return welcome;
+}
+
+const welcome = () => 'Welcome to Appleseeds Bootcamp!';
+const welcome = () => { return 'Welcome to Appleseeds Bootcamp!';};
+
+function power(a) {
+    let myNumber = a;
+    let result = Math.pow(myNumber, 2);
+    return result;
+}
+
+const power= (a) => a**2;
+const power=(a)=> { return a**2;}
+
+// From function expressions to IIFE functions.
+const squareRoot = a => Math.sqrt(a);
+
+((a)=> {let res= Math.sqrt(a);
+    console.log (res); })();
+
+const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+
+((a,b)=> {let res= Math.random() * (a - b) + b;
+console.log(res)})(2,3);
